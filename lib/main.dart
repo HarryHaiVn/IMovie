660b8886c2c688e38954c8afd3b851e1130226b8
harryhai <harryhaivn@gmail.com>
2020-07-25 09:09:39 +0700

    update source code

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'file:///D:/Project/flutter_app/lib/src/login/login_page.dart';
+import 'package:flutterapp/src/login/login_page.dart';
 
 void main() {
   runApp(MaterialApp(
